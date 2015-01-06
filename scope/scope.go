87c01ad616d89a34afd8b7111b348cc8dbf736a6
Quinn Slack <qslack@qslack.com>
2015-01-05 16:22:10 -0800

    add more scope (local/exported/etc.) test cases

--- scope/scope.go
+++ scope/scope.go
@@ -1,15 +1,74 @@
 // This package tests Exported and Local.
 package scope
 
-// a - Exported: false, Local: false
+import (
+	// _: local
+	_ "fmt"
+
+	// a: local
+	s "strings"
+)
+
+// Comments indicate the desired scope. The default is unexported and
+// nonlocal. Exported and local defs are marked as such.
+
+// a: (unexported)
 var a bool
 
-// A - Exported: true, Local: false
+// A: exported
 var A bool
 
-// fn - Exported: false, Local: false
+// fn
 func fn() {
-	// b - Exported: false, Local: true
+	// b: local
 	var b bool
 	_ = b
+
+	// T0: local
+	// T0.f: local
+	type T0 struct{ f string }
+
+	// x: local
+	for _, x := range []int{} {
+		// y: local
+		y := x
+		_ = y
+
+		// Y: local
+		var Y int
+		_ = Y
+
+		// T1: local
+		// T1.f: local
+		type T1 struct{ f string }
+	}
+}
+
+// T2: exported
+type T2 struct{}
+
+// F: exported
+// a: local
+// b: local
+func (T2) F(a string) (b string) {
+	return
+}
+
+// g: exported type, unexported method
+func (T2) g() {
+	return
+}
+
+// Unexported type, exported methods
+
+type t3 struct{}
+
+// F: exported
+func (t3) F() {}
+
+// g: unexported
+func (t3) g() {}
+
+func init() {
+	_ = s.Replace
 }
